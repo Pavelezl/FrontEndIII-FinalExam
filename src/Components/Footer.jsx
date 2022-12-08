4ade8dbab7c3f8b05aac39d9e438748d96387d52
Pablo Velez <pablovelezlopera3@gmail.com>
2022-12-08 17:46:40 -0500

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+// import { footer } from "./styleComponents/footer";
+// import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
-    </footer>
-  )
-}
+    <div>
+      <footer>
+        <img src="public\images\DH.png" alt="" />
+          <div>
+            <img src="public\images\ico-facebook.png" alt="" />
+            <img src="public\images\ico-instagram.png" alt="" />
+            <img src="public\images\ico-whatsapp.png" alt="" />
+            <img src="public\images\ico-tiktok.png" alt="" />
+          </div>
 
-export default Footer
+        <div style={{ background: "red", with: "100%" }}>
+          <p
+            className="back-to-top"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            BACK TO TOP
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
